f27af2d3c4db91748256f5cecf1b2ad7f3bc6143
Andrés Arnedo <andresarnedo2002@gmail.com>
2022-05-21 17:32:41 -0500

    Ruta de foto de perfil en {edit}

--- routes/users.js
+++ routes/users.js
@@ -15,76 +15,63 @@ const connectionBD = mysql.createConnection({
 });
 
 /* GET users listing. */
-router.get('/auth', Auth,(req, res, next)=>{
-  res.send("Prueba")
-})
+router.get("/auth", Auth, (req, res, next) => {
+  res.send("Prueba");
+});
 
-router.post("/profilepic",Auth, upload.single('image'), async function (req,res){
-
-  try{
+router.post(
+  "/edit",
+  Auth,
+  upload.single("image"),
+  async function (req, res, next) {
     const image = await cloudinary.uploader.upload(req.file.path);
     const imagePath = image.secure_url;
-    res.json(imagePath);
-    let { id , role} = req.body;
-    let sql = `UPDATE ${role} SET foto_perfil='${imagePath}' WHERE id='${id}'`;
-    await connectionBD.query(sql, function (error, results){
-      if(error){
-        console.log(error);
-        res.send({
-          success: false
-        })
-      }else{
-        res.send({
-          message:'Foto de perfil actualizada',
-          success: true
-        })
-      }
-    })
-  } catch (err){
-    console.log(err)
-  }
-
-} )
-
-router.post("/edit", Auth, async function (req, res, next) {
-  if (req.body.role == "clientes") {
-    let { id, telefono, password } = req.body;
-    password = bcrypt.hashSync(password, 10);
-    let sql = `UPDATE clientes SET telefono='${telefono}',password='${password}' WHERE id='${id}'`;
-    console.log(sql)
-    await connectionBD.query(sql, function (error, results) {
-      if (error) {
-        console.log(error);
-        res.send({
-          success: false
-        });
-      } else {
-        res.send({
-          message: "Modificado correctamente",
-          success: true,
-        });
-      }
-    });
-  } else {
-    let { id, password, tipoServicio, detalleServicio, tarifaHora, telefono } = req.body; 
-    password = bcrypt.hashSync(password, 10);
-    let sql = `UPDATE trabajadores SET 
+    if (req.body.role == "clientes") {
+      let { id, telefono, password } = req.body;
+      password = bcrypt.hashSync(password, 10);
+      let sql = `UPDATE clientes SET telefono='${telefono}',password='${password}',foto_perfil='${imagePath}' WHERE id='${id}'`;
+      console.log(sql);
+      await connectionBD.query(sql, function (error, results) {
+        if (error) {
+          console.log(error);
+          res.send({
+            success: false,
+          });
+        } else {
+          res.send({
+            message: "Modificado correctamente",
+            success: true,
+          });
+        }
+      });
+    } else {
+      let {
+        id,
+        password,
+        tipoServicio,
+        detalleServicio,
+        tarifaHora,
+        telefono,
+      } = req.body;
+      password = bcrypt.hashSync(password, 10);
+      let sql = `UPDATE trabajadores SET 
     password='${password}',tipo_servicio='${tipoServicio}',
-    detalle_servicio='${detalleServicio}',tarifa_hora=${tarifaHora},telefono='${telefono}' WHERE id='${id}'`;
-    await connectionBD.query(sql, function (error, results) {
-      if (error) {
-        console.log(error);
-        res.send({
-          success: false,
-        });
-      } else {
-        res.send({
-          message: "Modificado correctamente",
-          success: true,
-        });
-      }
-    });
+    detalle_servicio='${detalleServicio}',tarifa_hora=${tarifaHora},telefono='${telefono}',foto_perfil='${imagePath}' WHERE id='${id}'`;
+      await connectionBD.query(sql, function (error, results) {
+        if (error) {
+          console.log(error);
+          res.send({
+            success: false,
+          });
+        } else {
+          res.send({
+            message: "Modificado correctamente",
+            success: true,
+          });
+        }
+      });
+    }
   }
-});
+);
 
 module.exports = router;
